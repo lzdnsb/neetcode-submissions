d6f4529fd952a50e193abcbb70dfa6da42e0753e
lzdnsb <64148708+lzdnsb@users.noreply.github.com>
2026-05-09 16:28:32 -0700

    Add: insert-new-interval - submission-5

---
+++ Data Structures & Algorithms/insert-new-interval/submission-5.py
@@ -0,0 +1,43 @@
+'''
+if end_i >= start_new or start_j <= end_new: overlap
+    start_merge = min(start_i, start_new)
+    end_merge = max(end_j, end_new)
+otherwise:
+    append the interval to the output list
+'''
+
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        output = []
+        i = 0
+        start_new, end_new = newInterval[0], newInterval[1]
+        while i < len(intervals):
+            start_i, end_i = intervals[i][0], intervals[i][1]
+            if end_i < start_new: # non-overlap
+                output.append(intervals[i])
+                i += 1
+            elif end_i >= start_new and start_i <= end_new: # overlap
+                if len(output) == 0 or (len(output) != 0 and output[-1][1] < min(start_i, start_new)):
+                    start_merge = min(start_i, start_new)
+                    end_merge = max(end_i, end_new)
+                    output.append([start_merge, end_merge])
+                    i += 1
+                else:
+                    output[-1][1] = max(output[-1][1], end_i)
+                    i += 1
+            else: # non-overlap and insert newInterval
+                if len(output) == 0:
+                    output.append(newInterval)
+                    output.append(intervals[i])
+                    i += 1
+                else:
+                    if not output or (start_new > output[-1][1] and end_new < start_i):
+                        output.append(newInterval)
+                    else:
+                        output.append(intervals[i])
+                        i += 1
+
+        # base case: start_new > intervals[-1][1]
+        if not intervals or start_new > intervals[-1][1]:
+            output.append(newInterval)
+        return output
